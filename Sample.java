cce5e7469b90f15d82880c8ac984fb95e485b47a
Akarsh-Chandrashekar <49974714+Akarsh-Chandrashekar@users.noreply.github.com>
2021-06-02 00:08:30 -0700

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,14 @@
 // Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+Hashmap - O(m) where m is length of chaining or length of lined list
+Queue using stack - O(n) 
 
+// Space Complexity :
+  Hashmap - O(n)
+  Queue using stack - O(2n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :
+ 
 
 // Your code here along with comments explaining your approach
+amotized O(1) approach for stack
+Hashmap contaons primary hashing array of length 10000 and a linked list in each of the index 
